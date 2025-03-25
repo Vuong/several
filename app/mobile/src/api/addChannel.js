617f920b4490c626e85e6efa2bfbfa3c0c1fb64b
vuong <vuongdo_777@yahoo.com>
2025-03-25 21:43:12 +0000

    Change database - improving performance

--- app/mobile/src/api/addChannel.js
+++ app/mobile/src/api/addChannel.js
@@ -1,8 +1,8 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function addChannel(server, token, type, data, cards ) {
-  const insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
-  const protocol = insecure ? 'http' : 'https';
+  var insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
+  var protocol = insecure ? 'http' : 'https';
   let params = { dataType: type, data: JSON.stringify(data), groups: [], cards };
   let channel = await fetchWithTimeout(`${protocol}://${server}/content/channels?agent=${token}`, { method: 'POST', body: JSON.stringify(params)} );
   checkResponse(channel);
