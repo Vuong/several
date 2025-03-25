6e8ec5c4a45cb8bf5d43123f53a2ee803da48ff0
vuong <vuongdo_777@yahoo.com>
2025-03-25 23:15:59 +0000

    Remove build - adding new feature

--- app/sdk/src/net/getCardDetail.ts
+++ app/sdk/src/net/getCardDetail.ts
@@ -2,8 +2,8 @@ import { checkResponse, fetchWithTimeout } from './fetchUtil';
 import { CardDetailEntity } from '../entities';
 
 export async function getCardDetail(node: string, secure: boolean, token: string, cardId: string): Promise<CardDetailEntity> {
-  const endpoint = `http${secure ? 's' : ''}://${node}/contact/cards/${cardId}/detail?agent=${token}`;
-  const detail = await fetchWithTimeout(endpoint, { method: 'GET' });
+  let endpoint = `http${secure ? 's' : ''}://${node}/contact/cards/${cardId}/detail?agent=${token}`;
+  let detail = await fetchWithTimeout(endpoint, { method: 'GET' });
   checkResponse(detail.status);
   return await detail.json();
 }
