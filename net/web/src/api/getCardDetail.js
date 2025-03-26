2c7ab81d818bba915caaaee397dec2d3fb138f53
vuong <vuongdo_777@yahoo.com>
2025-03-26 07:50:07 +0000

    Change tests - improving performance

--- net/web/src/api/getCardDetail.js
+++ net/web/src/api/getCardDetail.js
@@ -1,8 +1,8 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function getCardDetail(token, cardId) {
-  let param = "?agent=" + token
-  let detail = await fetchWithTimeout(`/contact/cards/${cardId}/detail${param}`, { method: 'GET' });
+  const param = "?agent=" + token
+  const detail = await fetchWithTimeout(`/contact/cards/${cardId}/detail${param}`, { method: 'GET' });
   checkResponse(detail);
   return await detail.json()
 }
